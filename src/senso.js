435c7e8af29902abb62cedbba002f349a6577aed
Adarsh Amirtham <adarsh@dividat.ch>
2016-09-05 17:27:14 +0200

    Set initial Mu to 20000

--- src/senso.js
+++ src/senso.js
@@ -27,7 +27,7 @@ function factory() {
 
     // Kalman Parameters
     senso.Q = new Plates(S.Matrix.Diagonal([0.0001, 0.0001, 2000]));
-    senso.mu = new Plates($V([0, 0, 0, 0]));
+    senso.mu = new Plates($V([20000, 20000, 20000, 20000]));
     senso.Sigma = new Plates(S.Matrix.Diagonal([100, 100, 100, 100]));
 
     senso.x = new Plates($V([0, 0, 0]));
