5d091ad63b04b45b6c30cdcabb16ae6912b6dc9e
Adarsh Amirtham <adarsh@dividat.ch>
2016-10-04 22:25:59 +0200

    don't permute while decoding.

--- src/senso/decode.js
+++ src/senso/decode.js
@@ -5,68 +5,10 @@ const SENSOR_BLOCKS = 1;
 var sylvester = require('sylvester');
 var Plates = require('./plates');
 
-// Permutations for sensor values in order to allign all plates
-var permutations = new Plates($M([
-    [
-        1, 0, 0, 0
-    ],
-    [
-        0, 1, 0, 0
-    ],
-    [
-        0, 0, 0, 1
-    ],
-    [0, 0, 1, 0]
-]), $M([
-    [
-        1, 0, 0, 0
-    ],
-    [
-        0, 1, 0, 0
-    ],
-    [
-        0, 0, 0, 1
-    ],
-    [0, 0, 1, 0]
-]), $M([
-    [
-        0, 0, 1, 0
-    ],
-    [
-        1, 0, 0, 0
-    ],
-    [
-        0, 1, 0, 0
-    ],
-    [0, 0, 0, 1]
-]), $M([
-    [
-        0, 0, 0, 1
-    ],
-    [
-        0, 0, 1, 0
-    ],
-    [
-        1, 0, 0, 0
-    ],
-    [0, 1, 0, 0]
-]), $M([
-    [
-        0, 1, 0, 0
-    ],
-    [
-        0, 0, 0, 1
-    ],
-    [
-        0, 0, 1, 0
-    ],
-    [1, 0, 0, 0]
-]))
-
 module.exports = function decode(buffer) {
 
     function decodePlate(buffer) {
-        return $V([buffer.readUInt16LE(0), buffer.readUInt16LE(2), buffer.readUInt16LE(4), buffer.readUInt16LE(6)]);
+        return $V([buffer.readInt16LE(0), buffer.readInt16LE(2), buffer.readInt16LE(4), buffer.readInt16LE(6)]);
     }
 
     // Get just the data block
@@ -79,10 +21,6 @@ module.exports = function decode(buffer) {
     var decoded = sensorBlocks.ap(new Plates(decodePlate));
     // var decoded = new Plates(decodePlate).ap(sensorBlocks);
 
-    // Permutes values
-    // TODO: this should be done using nicer, but I can't figure out how call() works...
-    var permuted = new Plates(permutations.center.multiply(decoded.center), permutations.up.multiply(decoded.up), permutations.right.multiply(decoded.right), permutations.down.multiply(decoded.down), permutations.left.multiply(decoded.left));
-
-    return permuted;
+    return decoded;
 
 }
