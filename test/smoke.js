c45b650d2f0f0fd0c9994b3f2cbd714acf0884d8
Johannes Emerich <johannes@emerich.de>
2017-06-01 12:22:13 +0200

    Use mocha for smoke tests

--- test/smoke.js
+++ test/smoke.js
@@ -1,14 +1,11 @@
-// TODO: use mocha - that will allow more flexible testing and output can be directly used for medtech documentation
 const request = require('request')
 
 const driverUrl = 'https://localhost.dividat.com:8380'
 
-request.get({url: driverUrl, timeout: 2000})
-  .on('response', (response) => {
-    console.log(`Established connection to driver on ${driverUrl}.`)
-    process.exit(0)
+describe('Server', () => {
+  it(`should accept connections at ${driverUrl}`, (done) => {
+    request.get({url: driverUrl, timeout: 2000})
+      .on('response', () => done())
+      .on('error', done)
   })
-  .on('error', (err) => {
-    console.error(err)
-    process.exit(1)
-  })
+})
